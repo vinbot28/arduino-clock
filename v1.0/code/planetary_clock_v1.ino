2d98ce59381ad9c852e8b6f77484e1c6a3f15d37
vinbot28 <11248lvin@gmail.com>
2025-05-16 15:03:42 -1000

    Update planetary_clock_v1.ino
    added comments

--- v1.0/code/planetary_clock_v1.ino
+++ v1.0/code/planetary_clock_v1.ino
@@ -1,11 +1,17 @@
-#include <Stepper.h>
+// Arduino library for controlling steppers
+#include <Stepper.h> 
 
-Stepper clock = Stepper(2038, 8, 10, 9, 11);
+// initializes a stepper named clock
+// with 2038 steps per revolution
+// and stepper driver connected on pins 8-11
+Stepper clock = Stepper(2038, 8, 10, 9, 11); 
 
 void setup() {
+	// sets the stepper to 1 rpm
 	clock.setSpeed(1);
 }
 
 void loop() {
+	// makes the stepper spin indefinitely 
 	clock.step(2038);
 }
